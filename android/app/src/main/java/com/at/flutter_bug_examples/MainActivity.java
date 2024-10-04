acd509d682bb2d3964f77b7add9ebcc479fdde54
a.teteryatnikov <a.teteryatnikov.sis@gmail.com>
2024-10-04 10:46:38 +0700

    openFile video bug ios example

---
+++ android/app/src/main/java/com/at/flutter_bug_examples/MainActivity.java
@@ -0,0 +1,6 @@
+package com.at.flutter_bug_examples;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _openFile,
+              child: Column(
+                children: [const Text('Try download and open video'), const Icon(Icons.play_arrow)],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openFile() async {
+    var path = (await getTemporaryDirectory()).path;
+
+    await Dio().download(
+      "https://file-examples.com/storage/fe36b23e6a66fc0679c1f86/2017/04/file_example_MP4_480_1_5MG.mp4",
+      '$path/file.mp4',
+    );
+
+    await OpenFile.open('$path/file.mp4');
+  }
+}
